cc296655a9588829953572b2b57282b9541ab73b
Ricardo Manuel Santana Saumeth <ricardosantana46@hotmail.com>
2018-04-07 01:10:15 +0100

    First css style added

--- src/container/App.js
+++ src/container/App.js
@@ -4,7 +4,34 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>Hello</h1>
+
+        <div className="wrapper">
+
+          <h1>Currency Converter</h1>
+
+          <div className="column col-3"> 
+            <select>
+              <option value="euro">United States Dollar</option>
+              <option value="USDollars">USD</option>
+              <option value="Jenes">JPY</option>
+              <option value="Niidea">BGN</option>
+            </select>
+          </div>
+
+          <div className="column col-3">
+            <img src="/arrow.png" alt="exchange-arrow"/>
+          </div>
+
+          <div className="column col-3">
+            <select>
+              <option value="euro">United States Dollar</option>
+              <option value="USDollars">USD</option>
+              <option value="Jenes">JPY</option>
+              <option value="Niidea">BGN</option>
+            </select>
+          </div>
+
+        </div>   
     );
   }
 }
